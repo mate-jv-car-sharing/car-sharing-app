a7592c53b98290c0449f1026be6a3c474633f160
Kukharenko Yurii <46027349+yurakukh@users.noreply.github.com>
2026-02-15 17:50:35 +0200

    [FEAT]: Create RentalAccessValidator to validate rental ownership

---
+++ src/main/java/com/example/carsharing/service/RentalAccessValidator.java
@@ -0,0 +1,25 @@
+package com.example.carsharing.service;
+
+import com.example.carsharing.exception.RentalException;
+import com.example.carsharing.model.Rental;
+import com.example.carsharing.model.User;
+import com.example.carsharing.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RentalAccessValidator {
+    private final UserService userService;
+
+    public void validateRentalOwnership(User user, Rental rental) {
+        boolean isManager = userService.isManager(user);
+        if (!isManager && !rental.getUser().getId().equals(user.getId())) {
+            throw new RentalException(String.format(
+                    "User with id %d is not the owner of the rental with id %d",
+                    user.getId(),
+                    rental.getId()
+            ));
+        }
+    }
+}
