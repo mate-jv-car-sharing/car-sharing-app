5867473c223942a2397bb0e9323fdbe390dadf5c
Kukharenko Yurii <46027349+yurakukh@users.noreply.github.com>
2026-02-15 19:54:59 +0200

    [FEAT]: Add a PaymentException exception to provide a convenient way to handle payment exceptions.

---
+++ src/main/java/com/example/carsharing/exception/PaymentException.java
@@ -0,0 +1,10 @@
+package com.example.carsharing.exception;
+
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
+    public PaymentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
